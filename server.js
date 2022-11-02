c436e47fbe96196e4f60b79b30e4acedeac1fc35
Brandon <hillbp2014@gmail.com>
2022-11-02 13:49:57 -0400

    added listener to server

--- server.js
+++ server.js
@@ -4,4 +4,10 @@ console.log(process.env);
 const express = require("express");
 const sql = require("mysql2");
 
+const PORT = process.env.PORT || 3001;
+
 const app = express();
+
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`)
+})
