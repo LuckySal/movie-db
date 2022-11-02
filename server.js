e779c9704326b2d3a623946075074d3b7f332ff6
Brandon <hillbp2014@gmail.com>
2022-11-02 13:53:43 -0400

    added sql connection

--- server.js
+++ server.js
@@ -4,6 +4,13 @@ console.log(process.env);
 const express = require("express");
 const sql = require("mysql2");
 
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : process.env.password,
+    database : 'movies_db'
+});
+
 const PORT = process.env.PORT || 3001;
 
 const app = express();
